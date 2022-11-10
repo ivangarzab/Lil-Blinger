e8528f6565e75dd72b1a844013f89423705ef604
Iván Garza Bermea <ivangb6@gmail.com>
2022-11-10 08:53:22 -0800

    Update main.py for deployment

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-# This example requires the 'message_content' intent. UPDATE: Does it?
-
 import discord
+import random
+import env
+
+DEFAULT_CHANNEL_BLINGERS = 485714188073697296
 
 intents = discord.Intents.default()
-#intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+greetings = ["Hallo", "suuup", "buenas"]
 
 @client.event
 async def on_ready():
@@ -14,41 +17,78 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return
+        return # Avoid the bot responding to itself\
+    
+    author = message.author.mention
+    msgFormat = message.content.lower()
+    allowed_mentions = discord.AllowedMentions(everyone = True)
 
+    shouts = [f"{author} is looking for love", f"{author} cannot find any friends", f"{author} is on comms!"]
     if '<' == message.content:
-      await message.channel.send(f"{message.author.mention} is looking for love")
+      await message.channel.send(shouts[random.randint(0, 2)])
     if '<<' == message.content:
-      await message.channel.send(f"{message.author.mention} cannot find any friends")
+      await message.channel.send(shouts[random.randint(0, 2)])
     if '<--' == message.content:
-      await message.channel.send(f"{message.author.mention} is on comms!")
+      await message.channel.send(shouts[random.randint(0, 2)])
     if '<---' == message.content:
-      await message.channel.send(f"{message.author.mention} is on comms!")
-    if 'blinger' in message.content:
-      await message.channel.send('BLINGERS PILINGERS ASSEMBLE!!')
-    if 'puto' in message.content:
-      await message.channel.send(f"puto tu {message.author.mention}")
-    if 'madre' in message.content:
-      await message.channel.send(f"la tuya {message.author.mention}")
-    if 'shit' in message.content:
-      await message.channel.send(f"A que hora pasas por el pan {message.author.mention}")
-    if 'verga' in message.content:
-      await message.channel.send(f"{message.author.mention} tiene hambre")
-    if 'tibia' in message.content:
+      await message.channel.send(shouts[random.randint(0, 2)])
+    
+    if 'blingers' in msgFormat:
+      await message.channel.send('BLINGERS PILINGERS ASSEMBLE:bangbang:')
+    if 'lil blinger' in msgFormat:
+        await message.channel.send(greetings[random.randint(0,2)])
+    if 'hola' in msgFormat:
+      await message.channel.send(greetings[random.randint(0,2)])
+    if 'GAME' in message.content:
+      await message.channel.send(f"echele! @everyone")
+    if 'game' in message.content:
+      await message.channel.send(f"echele!")
+    if 'sup' in msgFormat:
+      await message.channel.send(f"sup")
+    if 'date' in msgFormat:
+      await message.channel.send(f"lego")
+    if 'marco' in msgFormat:
+      await message.channel.send(f"POLO!")
+    
+    if 'puto' in msgFormat:
+      await message.channel.send(f"puto tu {author}")
+    if 'madre' in msgFormat:
+      await message.channel.send(f"la tuya {author}")
+    if 'shit' in msgFormat:
+      await message.channel.send(f"A que hora pasas por el pan {author}")
+    if 'verga' in msgFormat:
+      await message.channel.send(f"{author} tiene hambre")
+    if 'tibia' in msgFormat:
       await message.channel.send(f"sucks")
-    if 'nabbot' in message.content:
-      await message.channel.send(f"SUCKS BALLS!")
-    if 'golf' in message.content:
-      await message.channel.send(f"culean")
-    if 'help' == message.content:
+    if 'nabbot' in msgFormat:
+      await message.channel.send(f"el NabBot mlp :smiling_imp:")
+    if 'golf' in msgFormat:
+      await message.channel.send(f"te culeas")
+    if 'help' == msgFormat:
       await message.channel.send('pelas')
-    if 'share' in message.content:
+    if 'share' in msgFormat:
       await message.channel.send('comparte esta')
-    if 'soo' in message.content:
+    if 'soo' in msgFormat:
       await message.channel.send('.........')
+    if 'culean' in msgFormat:
+      await message.channel.send('aqui nadie culea! :metal:')
+    if 'culeas' in msgFormat:
+      await message.channel.send(f"tu culeas {author}")
+    if 'traen nada' in msgFormat:
+      await message.channel.send(f"tu no traes nada! {author}")
+    if 'voy' in msgFormat:
+      await message.channel.send(f"que se vea")
 
-client.event
+@client.event
 async def on_message_delete(message):
-    await message.channel.send(f"{message.author.mention}, why would you delete the message '{message}'?")
+    await message.channel.send(f"{message.author.mention}, why would you delete that message?")
+    
+@client.event
+async def on_member_join(member):
+    println(f"{member} joined")
+    channel = client.get_channel(DEFAULT_CHANNEL_BLINGERS)
+    if not channel:
+        return
+    await channel.send(f"Welcome {member}!")
 
-client.run('MTAzOTMxNzg2NTA0NTY4ODM4MQ.GlsEI7.ZjB9dMY3c30GQE4m9uQz8wfDNkBi7br5FTmFuA')
+client.run(env.TOKEN)
