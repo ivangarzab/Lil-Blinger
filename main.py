12fd1b5db02e929296cc5643f87566ce157dbb76
Iván Garza Bermea <ivangb6@gmail.com>
2022-11-11 09:10:22 -0800

    Add '..' & 'arre' responses and edit 'help'

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ async def on_message(message):
       await message.channel.send(shouts[random.randint(0, 2)])
     if '<---' == message.content:
       await message.channel.send(shouts[random.randint(0, 2)])
+    if '...' == message.content:
+        await message.channel.send(f"te falta algo {author}?")
     
     if 'blingers' in msgFormat:
       await message.channel.send('BLINGERS PILINGERS ASSEMBLE:bangbang:')
@@ -49,6 +51,8 @@ async def on_message(message):
       await message.channel.send(f"lego")
     if 'marco' in msgFormat:
       await message.channel.send(f"POLO!")
+    if 'arre' in msgFormat:
+      await message.channel.send(f"arre vaquero!")
     
     if 'puto' in msgFormat:
       await message.channel.send(f"puto tu {author}")
@@ -65,7 +69,7 @@ async def on_message(message):
     if 'golf' in msgFormat:
       await message.channel.send(f"te culeas")
     if 'help' == msgFormat:
-      await message.channel.send('pelas')
+      await message.channel.send(f"nadie te va a ayudar {author}!")
     if 'share' in msgFormat:
       await message.channel.send('comparte esta')
     if 'soo' in msgFormat:
